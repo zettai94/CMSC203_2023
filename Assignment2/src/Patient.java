612bd49a2b135b52b8b9be5926d104c26bb5030b
zettai94 <128860135+zettai94@users.noreply.github.com>
2023-09-22 20:20:03 -0400

    Add files via upload

---
+++ Assignment2/src/Patient.java
@@ -0,0 +1,187 @@
+/*
+ * Class: CMSC203 
+ * Instructor: Professor Khandan Monshi
+ * Description: Write a class named Patient with fields for required data.
+ * Due: 09/26/223
+ * Platform/compiler: Eclipse IDE
+ * I pledge that I have completed the programming 
+ * assignment independently. I have not copied the code 
+ * from a student or any source. I have not given my code 
+ * to any student.
+   Print your Name here: Silvia Lim Siew Ling
+*/
+
+public class Patient
+{
+	//attributes
+	private String firstName, midName, lastName, add, city, state, zip, emerName, phone, emerPhone;
+	
+	//no arg constructor
+	public Patient()
+	{
+		
+	}
+	
+	//parameterized constructor initializes patient's first, mid, last name to given values
+	public Patient(String fName, String mName, String lName)
+	{
+		this.firstName = fName;
+		this.midName = mName;
+		this.lastName = lName;
+	}
+	
+	//parameterized constructor that initializes all attributes to given values
+	public Patient(String fName, String mName, String lName, String add, String city, String state, 
+					String zip, String emerName, String phone, String emerPhone)
+	{
+		this.firstName = fName;
+		this.midName = mName;
+		this.lastName = lName;
+		this.add = add;
+		this.city= city;
+		this.state = state;
+		this.zip = zip;
+		this.emerName = emerName;
+		this.phone = phone;
+		this.emerPhone = emerPhone;
+	}
+	
+	//buildFullName method that returns string which is the concatenation of first, mid, last name separated by space
+	public String buildFullName()
+	{
+		String fullName;
+		fullName = firstName + " " + midName + " " + lastName;
+		return fullName;
+	}
+	
+	//buildAddress method that returns string which is the concatenation of address, city, state and zip separated by space
+	public String buildAddress()
+	{
+		String address;
+		address = add + " " + city + " " + state + " " + zip;
+		return address;
+	}
+	
+	//buildEmergencyContact method that returns String which is concatenation of emergency number and 
+	//emergency phone separated by space
+	public String buildEmergencyContact()
+	{
+		String emerContact;
+		emerContact = emerName + " " + emerPhone;
+		return emerContact;
+	}
+	
+	/* accessor for first name, mid name, last name,
+	 * address, city, state, zip and emergency name*/
+	public String getFirstName()
+	{
+		return firstName;
+	}
+
+	public String getMidName()
+	{
+		return midName;
+	}
+	
+	public String getLastName()
+	{
+		return lastName;
+	}
+	
+	public String getAddress()
+	{
+		return add;
+	}
+	
+	public String getCity()
+	{
+		return city;
+	}
+	
+	public String getState()
+	{
+		return state;
+	}
+	
+	public String getZip()
+	{
+		return zip;
+	}
+	
+	public String getEmerName()
+	{
+		return emerName;
+	}
+	
+	public String getPhone()
+	{
+		return phone;
+	}
+	
+	public String getEmerPhone()
+	{
+		return emerPhone;
+	}
+	
+	
+	/* mutator for first name, mid name, last name,
+	 * address, city, state, zip and emergency name*/
+	public void setFirstName(String fName)
+	{
+		this.firstName = fName;
+	}
+	
+	public void setMidName(String mName)
+	{
+		this.midName = mName;
+	}
+	
+	public void setLastName(String lName)
+	{
+		this.lastName = lName;
+	}
+	
+	public void setAdd(String add)
+	{
+		this.add = add;
+	}
+	
+	public void setCity(String city)
+	{
+		this.city = city;
+	}
+	
+	public void setState(String state)
+	{
+		this.state = state;
+	}
+	
+	public void setZip(String zip)
+	{
+		this.zip = zip;
+	}
+	
+	public void setEmerName(String emerName)
+	{
+		this.emerName = emerName;
+	}
+	
+	public void setPhone(String phone)
+	{
+		this.phone = phone;
+	}
+	
+	public void setEmerPhone(String emerPhone)
+	{
+		this.emerPhone = emerPhone;
+	}
+	
+	//toString method that display all information of patient
+	public String toString()
+	{
+		String output = "\tName: " + buildFullName() + "\n\tAddress:  " + buildAddress() + "\n\tEmergency Contact: "
+							+ buildEmergencyContact();
+		return output;
+	}
+	
+}
---
+++ Assignment2/src/PatientDriverApp.java
@@ -0,0 +1,63 @@
+/*
+ * Class: CMSC203 
+ * Instructor: Professor Khandan Monshi
+ * Description: Write a class named PatientDriverApp that creates an instance for Patient class and 3 instances for Procedure class.
+ * Due: 09/26/223
+ * Platform/compiler: Eclipse IDE
+ * I pledge that I have completed the programming 
+ * assignment independently. I have not copied the code 
+ * from a student or any source. I have not given my code 
+ * to any student.
+   Print your Name here: Silvia Lim Siew Ling
+*/
+
+public class PatientDriverApp 
+{
+	public static void main(String[] args)
+	{
+		//instance for patient class
+		Patient p1 = new Patient("Jenny", "Elaine", "Santori", "123 Main Street", "MyTown", "CA", "01234",
+									"Bill Santori", "100-222-3456", "777-555-1212");
+			
+		//3 instances for procedure class
+		Procedure proc1 = new Procedure("Physical Exam", "07/20/2019", "Dr. Irvine", 3250.00);
+		Procedure proc2 = new Procedure("X-ray", "07/20/2019", "Dr. Jamison", 5500.43);
+		Procedure proc3 = new Procedure("Blood Test", "07/20/2019", "Dr. June", 1400.75);
+		
+		//call methods
+		displayPatient(p1);
+		displayProcedure(proc1);
+		displayProcedure(proc2);
+		displayProcedure(proc3);
+		System.out.printf("\nTotal Charges: %,.2f", calculateTotalCharges(proc1, proc2, proc3));
+		//display total charges of procedures in comma separated with 2 decimal points
+				
+		//student name, MC number and due date
+		String studentName = "Silvia Lim Siew Ling";
+		String mcNumber = "M21157481";
+		String dueDate = "09/26/2023";
+		System.out.println("\n\nStudent Name: " + studentName + "\nMC#: " + mcNumber + "\nDue Date: " + dueDate);
+				 	
+	}
+			
+	//displayPatient method given patient object, display patient's information
+	public static void displayPatient(Patient p1)
+	{	
+		System.out.println("Patient info: ");
+		System.out.println(p1.toString());		
+	}
+			
+	//displayProcedure method given procedure object, display procedure's information
+	public static void displayProcedure(Procedure proc)
+	{
+			System.out.println(proc);
+	}
+	
+	//calculateTotalCharges method that takes 3 procedures as parameter and return total charges of these procedures		
+	public static double calculateTotalCharges(Procedure proc1, Procedure proc2, Procedure proc3 )
+	{
+		double totalPrice = (proc1.getPrice() + proc2.getPrice() + proc3.getPrice());
+		return totalPrice;
+	}
+	
+}
